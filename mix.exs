e7a7cd9bf401a5739650060e3b98d846a59543a3
Andrew Summers <oasummerso@gmail.com>
2020-10-20 20:25:19 -0500

    fix: Maybe this will work? (#3)

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule GhActionsTesting.MixProject do
   use Mix.Project
 
-  @version "0.0.1"
+  @version "0.1.0"
   @repo_url "https://github.com/asummers/gh_actions_testing"
 
   def project do
